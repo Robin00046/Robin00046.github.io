687921179d3ac129e8560a74ce3b7b7243355d12
Robin00046 <abdulrofiqalmuqorobin@gmail.com>
2023-09-04 18:30:53 +0700

    navbar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router";
-import PhosphorIcons from "@phosphor-icons/vue";
-import VuePageTransition from "vue-page-transition";
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import PhosphorIcons from '@phosphor-icons/vue';
 
-createApp(App)
-  .use(router)
-  .use(PhosphorIcons)
-  .use(VuePageTransition)
-  .mount("#app");
+createApp(App).use(router).use(PhosphorIcons).mount('#app');
