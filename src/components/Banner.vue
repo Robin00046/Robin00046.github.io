a30cb6fc8e83893005dc42d1d9a085d18c52cc5f
Robin00046 <abdulrofiqalmuqorobin@gmail.com>
2023-09-11 11:02:34 +0700

    sudah

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,7 +7,7 @@
       <h1
         class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase"
       >
-        Hi, Iam Atika Cantik
+        Hi, Iam Robin
       </h1>
       <p
         class="font-general-medium mt-2 text-lg sm:text-xl xl:text-2xl text-center sm:text-left leading-none text-gray-400"
