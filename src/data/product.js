9f9c1a257c0821fc394fcd5a231d1426926dd914
Robin00046 <abdulrofiqalmuqorobin@gmail.com>
2024-02-22 21:48:42 +0700

    tambah portofolio

--- src/data/product.js
+++ src/data/product.js
@@ -1,5 +1,9 @@
 import porto from "../assets/portofolio.png";
 import mbkm from "../assets/mbkm.png";
+import slicing from "../assets/slicing.png";
+import slicing3 from "../assets/slicing3.png";
+import slicing2 from "../assets/slicing2.png";
+import doaterbaik from "../assets/doaterbaik.png";
 export default [
   {
     id: 1,
@@ -18,7 +22,33 @@ export default [
     ],
 
     description: `
-      <p>Membuat sistem yang terintegrasi dengan bermacam macam sistem yang ada dikampus </p>
+      <p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:normal;font-size:15px;font-family:"Calibri","sans-serif";text-align:justify;'><span style='font-size:16px;font-family:"Times New Roman","serif";color:#0D0D0D;background:white;'>Sistem dashboard portofolio adalah antarmuka visual yang menyajikan informasi kunci tentang suatu portofolio secara ringkas dan mudah dimengerti. Ini memberikan pandangan holistik terhadap kinerja, status, dan metrik penting terkait dengan portofolio tersebut.</span></p>
+    <p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:normal;font-size:15px;font-family:"Calibri","sans-serif";text-align:justify;'><span style='font-size:16px;font-family:"Times New Roman","serif";color:black;'>Berikut Fitur Fitur Yang terdapat pada sistem tersebut.</span></p>
+    <ul style="list-style-type: disc; margin-left: 30px;">
+        <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Otorisasi Akses</span></strong>
+            <ol style="list-style-type: circle; margin-left: 35px;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Memastikan bahwa hanya pengguna yang berwenang yang dapat mengakses informasi tertentu dalam dashboard.</span></li>
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Terdapat Beberapa Hak Akses Pada Sistem Tersebut.</span></li>
+            </ol>
+        </li>
+        <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Pengambilan Data Langsung</span></strong>
+            <ol style="list-style-type: circle; margin-left: 35px;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Integrasi dengan sumber data eksternal seperti pasar keuangan atau alat manajemen proyek.</span></li>
+            </ol>
+        </li>
+        <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Tabel Data</span></strong>
+            <ol style="list-style-type: circle; margin-left: 35px;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Menyajikan informasi lebih rinci tentang setiap table yang dibutuhkan Universitas.</span></li>
+            </ol>
+        </li>
+        <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Grafik atau Diagram</span></strong>
+            <ol style="list-style-type: circle; margin-left: 35px;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Menyajikan gambaran umum kinerja portofolio dengan grafik batang.</span></li>
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Memberikan informasi visual tentang perkembangan nilai atau kinerja portofolio seiring waktu</span></li>
+            </ol>
+        </li>
+    </ul>
+    <p><em>NB:Mohon Maaf Tidak Menyertakan Screenshot Karena Sistem Bersifat Private</em></p>
     `,
   },
   {
@@ -38,9 +68,88 @@ export default [
     ],
 
     description: `
-      <p>
-      Membuat sistem mbkm kampus yang memiliki fitur untuk pendaftaran input krs dan lain lain
-      </p>
+    <p>&quot;Sistem Merdeka Belajar - Kampus Merdeka&quot; adalah konsep yang diperkenalkan oleh Kementerian Pendidikan dan Kebudayaan Republik Indonesia. Ini bertujuan untuk memberikan kebebasan kepada mahasiswa dalam merancang kurikulum, memilih mata kuliah, dan menentukan jalur pendidikan sesuai dengan minat dan bakat masing-masing.&nbsp;</p>
+    <p>Berikut Fitur Fitur Yang terdapat pada sistem tersebut.</p>
+    <ul style="list-style-type: disc; margin-left: 30px;">
+    <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Otorisasi Akses</span></strong>
+        <ol style="list-style-type: circle; margin-left: 35px;">
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Memastikan bahwa hanya pengguna yang berwenang yang dapat mengakses informasi tertentu dalam dashboard.</span></li>
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Terdapat Beberapa Hak Akses Pada Sistem Tersebut.</span></li>
+        </ol>
+    </li>
+    <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Manajemen Akademik</span></strong>
+        <ol style="list-style-type: circle; margin-left: 35px;">
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'><strong>Pendaftaran Mahasiswa:</strong> Memfasilitasi proses pendaftaran mahasiswa MBKM.</span></li>
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'><strong>Pendaftaran KRS Mahasiswa:</strong> Memfasilitasi proses input Matakuliah Mahasiswa.</span></li>
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'><strong>Penjadwalan Kelas:</strong> Membantu dalam pengaturan jadwal perkuliahan dan pemberian mata kuliah</span></li>
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'><strong>Rekam Jejak Akademik:</strong> Melacak perkembangan akademik mahasiswa, termasuk nilai dan absensi.</span></li>
+        </ol>
+    </li>
+    <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Tabel Data</span></strong>
+        <ol style="list-style-type: circle; margin-left: 35px;">
+            <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>o	Menyajikan informasi Terkait Jumlah mahasiswa yang terdaftar, Daftar Mata Kuliah, Dan Nilai Pada setiap Mahasiswa berdasarkan matakuliah yang diambil.</span></li>
+        </ol>
+    </li>
+    
+</ul>
+    <p><em>NB:Mohon Maaf Tidak Menyertakan Screenshot Karena Sistem Bersifat Private</em></p>
+    `,
+  },
+  {
+    id: 3,
+    name: "Slicing Menggunakan Tailwind V3 dan Vue 3",
+    price: "$140",
+    href: "project",
+    rating: 5,
+    images: [
+      {
+        id: 1,
+        name: "Angled view",
+        src: slicing,
+        alt: "slicing",
+      },
+      {
+        id: 2,
+        name: "Angled view",
+        src: slicing2,
+        alt: "slicing",
+      },
+      {
+        id: 3,
+        name: "Angled view",
+        src: slicing3,
+        alt: "slicing",
+      },
+      // More images...
+    ],
+
+    description: `
+    <p>Melakukan Slicing atau pembuatan tampilan menggunakan Tailwind V3 dan Vue 3</p>
+    <p>Berikut Link Demo Slicing</p>
+    <a href="https://journal-topaz.vercel.app/archives" target="_blank" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Link Demo</a>
+    `,
+  },
+  {
+    id: 3,
+    name: "Website Undangan Pernikahan",
+    price: "$140",
+    href: "project",
+    rating: 5,
+    images: [
+      {
+        id: 1,
+        name: "Angled view",
+        src: doaterbaik,
+        alt: "doaterbaik",
+      },
+      // More images...
+    ],
+
+    description: `
+    <p>Membuat website undangan pernikahan dengan membeli domain dan hosting sendiri, serta melakukan konfigurasi pada server.</p>
+
+    <p>Berikut Link</p>
+    <a href="https://doaterbaik.com/" target="_blank" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Link Demo</a>
     `,
   },
   // {
--- src/data/productall.js
+++ src/data/productall.js
@@ -18,7 +18,32 @@ export default [
     ],
 
     description: `
-      <p>Membuat sistem yang terintegrasi dengan bermacam macam sistem yang ada dikampus </p>
+    <p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:normal;font-size:15px;font-family:"Calibri","sans-serif";text-align:justify;'><span style='font-size:16px;font-family:"Times New Roman","serif";color:#0D0D0D;background:white;'>Sistem dashboard portofolio adalah antarmuka visual yang menyajikan informasi kunci tentang suatu portofolio secara ringkas dan mudah dimengerti. Ini memberikan pandangan holistik terhadap kinerja, status, dan metrik penting terkait dengan portofolio tersebut.</span></p>
+    <p style='margin-top:0in;margin-right:0in;margin-bottom:10.0pt;margin-left:0in;line-height:normal;font-size:15px;font-family:"Calibri","sans-serif";text-align:justify;'><span style='font-size:16px;font-family:"Times New Roman","serif";color:black;'>Berikut Fitur Fitur Yang terdapat pada sistem tersebut.</span></p>
+    <ul style="list-style-type: disc;">
+        <li><strong><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Otorisasi Akses</span></strong>
+            <ol style="list-style-type: circle;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Memastikan bahwa hanya pengguna yang berwenang yang dapat mengakses informasi tertentu dalam dashboard.</span></li>
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Terdapat Beberapa Hak Akses Pada Sistem Tersebut.</span></li>
+            </ol>
+        </li>
+        <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Pengambilan Data Langsung</span>
+            <ol style="list-style-type: circle;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Integrasi dengan sumber data eksternal seperti pasar keuangan atau alat manajemen proyek.</span></li>
+            </ol>
+        </li>
+        <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Tabel Data</span>
+            <ol style="list-style-type: circle;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Menyajikan informasi lebih rinci tentang setiap table yang dibutuhkan Universitas.</span></li>
+            </ol>
+        </li>
+        <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Grafik atau Diagram</span>
+            <ol style="list-style-type: circle;">
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Menyajikan gambaran umum kinerja portofolio dengan grafik batang.</span></li>
+                <li><span style='line-height:115%;font-family:"Times New Roman","serif";font-size:16px;'>Memberikan informasi visual tentang perkembangan nilai atau kinerja portofolio seiring waktu</span></li>
+            </ol>
+        </li>
+    </ul>
     `,
   },
   {
